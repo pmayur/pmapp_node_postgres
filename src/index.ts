b735262025c25c1e141718e67afcc05b040d58a9
Mayur <thisispatil@gmail.com>
2021-09-03 15:59:58 +0530

    add: postgres db connection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { MainRouter } from './routers';
+import { createConnection } from 'typeorm';
+require('dotenv').config()
 
 const app = express();
 const router = MainRouter;
-const port = 8080; // default port to listen
+const port = process.env.HTTPSERVER_PORT || 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
@@ -16,3 +18,15 @@ app.use(router);
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 });
+
+createConnection({
+    type: 'postgres',
+    host: process.env.DBHOST,
+    port: 5432,
+    username: process.env.DBUSER_NAME,
+    password: process.env.DBPASSWORD,
+    database: process.env.DBNAME,
+    entities: [__dirname + '/entities/*'],
+}).then(() => {
+    console.log('postgres database connected')
+})
